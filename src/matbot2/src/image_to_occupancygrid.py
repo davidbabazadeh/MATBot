7d868ccbefc707f6f05354700ae8b593f1f50694
George Riera <griera@berkeley.edu>
2024-12-12 23:36:49 +0000

    Changed image to occupancy grid to load the image directly from a path

--- src/matbot2/src/image_to_occupancygrid.py
+++ src/matbot2/src/image_to_occupancygrid.py
@@ -2,23 +2,31 @@
 
 import rospy
 import cv2
-import numpy as np
+#import numpy as np
 from sensor_msgs.msg import Image
 from nav_msgs.msg import OccupancyGrid
 from cv_bridge import CvBridge
+import os
 
 class OccupancyGridMapNode:
     def __init__(self):
         rospy.init_node('occupancy_grid_map_node', anonymous=True)
         self.bridge = CvBridge()
-        self.image_subscriber = rospy.Subscriber("/input_image", Image, self.image_callback)
-        self.occupancy_grid_publisher = rospy.Publisher('/occupancy_grid', OccupancyGrid, queue_size=10)
-        rospy.spin()
 
-    def image_callback(self, msg):
+         # Initialize publisher
+        self.occupancy_grid_publisher = rospy.Publisher('/occupancy_grid', OccupancyGrid, queue_size=10)
+
+        #self.image_subscriber = rospy.Subscriber("/input_image", Image, self.image_callback)
+        # Load the image
+        image_path = os.path.expanduser('~/MATBot/saved_maps/imagemap.jpg')
+        cv_image = cv2.imread(image_path)
+        if cv_image is None:
+            rospy.logerr(f"Failed to load image from path: {image_path}")
+            return
+
+        
+        # Convert ROS Image message to OpenCV format
         try:
-            # Convert ROS Image message to OpenCV format
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             
             # Convert to grayscale
             gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
@@ -31,9 +39,31 @@ class OccupancyGridMapNode:
 
             # Publish the occupancy grid
             self.occupancy_grid_publisher.publish(occupancy_grid)
-
         except Exception as e:
             rospy.logerr(f"Error in image_callback: {e}")
+
+        
+        rospy.spin()
+
+    # def image_callback(self, msg):
+    #     try:
+    #         # Convert ROS Image message to OpenCV format
+    #         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            
+    #         # Convert to grayscale
+    #         gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+    #         # Thresholding to create a binary image
+    #         _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY_INV)
+
+    #         # Create occupancy grid from binary image
+    #         occupancy_grid = self.create_occupancy_grid(binary_image)
+
+    #         # Publish the occupancy grid
+    #         self.occupancy_grid_publisher.publish(occupancy_grid)
+
+    #     except Exception as e:
+    #         rospy.logerr(f"Error in image_callback: {e}")
 
     def create_occupancy_grid(self, binary_image):
         occupancy_grid = OccupancyGrid()
